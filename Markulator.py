687f7ef906ed99c028e96ca5e0bfcc1e65917810
dovshmi <dovshmi@gmail.com>
2023-12-05 11:20:28 +0200

    change the main function

--- Markulator.py
+++ Markulator.py
@@ -1,29 +1,29 @@
-def mm_to_inches(mm_value):
-    # 1 millimeter is approximately equal to 0.0393701 inches
-    inches_value = mm_value * 0.0393701
-    return inches_value
+def inches_to_mm(inches_value):
+    # 1 inch is approximately equal to 25.4 millimeters
+    mm_value = inches_value * 25.4
+    return mm_value
 
 def main():
-    # Get user input for millimeter value
-    mm_input = float(input("Enter millimeter value: "))
+    # Get user input for inches value
+    inches_input = float(input("Enter inches value: "))
 
     # Get user input for tolerance range
     min_tolerance = float(input("Enter minimum tolerance: "))
     max_tolerance = float(input("Enter maximum tolerance: "))
 
-    # Calculate inches with tolerance
-    rounded_inches = round(mm_to_inches(mm_input), 2)
-    non_rounded_inches = mm_to_inches(mm_input)
+    # Calculate millimeters with tolerance
+    rounded_mm = round(inches_to_mm(inches_input), 2)
+    non_rounded_mm = inches_to_mm(inches_input)
 
     # Check for deviation and adjust non-rounded value if necessary
-    deviation = abs(rounded_inches - non_rounded_inches)
-    if deviation > 0.005:  # Adjust if deviation is more than 0.005 inches
-        non_rounded_inches = rounded_inches
+    deviation = abs(rounded_mm - non_rounded_mm)
+    if deviation > 0.005:  # Adjust if deviation is more than 0.005 millimeters
+        non_rounded_mm = rounded_mm
 
     # Display the results
-    print(f"{mm_input} millimeters is approximately:")
-    print(f"Rounded: {rounded_inches} inches")
-    print(f"Non-rounded: {non_rounded_inches} inches")
+    print(f"{inches_input} inches is approximately:")
+    print(f"Rounded: {rounded_mm} millimeters")
+    print(f"Non-rounded: {non_rounded_mm} millimeters")
 
 if __name__ == "__main__":
     main()
