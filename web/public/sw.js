ef992e11a654c1cd7025f4b6679c6f2fafac731d
Rony Shmidov <70490582+Dovshmi@users.noreply.github.com>
2026-06-28 18:03:22 +0300

    Refresh cache for language layout fixes

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'markulator-v091-language-cache';
+const CACHE_NAME = 'markulator-v091-language-layout-cache';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/markulator-icon.svg'];
 
 self.addEventListener('install', (event) => {
