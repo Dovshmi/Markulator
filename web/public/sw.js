5724b08ac95c226ce83d8b7a08330f68e14f4823
Rony Shmidov <70490582+Dovshmi@users.noreply.github.com>
2026-06-28 17:32:37 +0300

    Refresh service worker cache for v0.9.1

--- web/public/sw.js
+++ web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'markulator-v09-cache-2';
+const CACHE_NAME = 'markulator-v091-cache';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/markulator-icon.svg'];
 
 self.addEventListener('install', (event) => {
